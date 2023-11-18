dd3d0b0fff8e2c7598ea131068d97796c3462782
SOMON Bastian <bastian.somon@gmail.com>
2023-11-18 23:15:10 +0100

    Fix infra

--- infra/secrets/create_registry_secret.sh
+++ infra/secrets/create_registry_secret.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-then
-  echo "Spécifiez un namespace pour créer les secrets"
-  exit 1
-fi
-
 kubectl create secret generic registry-creds \
     --from-file=.dockerconfigjson=/root/.docker/config.json \
     --type=kubernetes.io/dockerconfigjson \
-    -n=$1
+    -n=city-connect
