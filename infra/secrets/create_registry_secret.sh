21980d0d54e9a9d60f1e29214fba8790e579a487
Bastian SOMON <bastian.somon@gmail.com>
2024-09-09 21:06:02 +0000

    CI TRIGGER

--- infra/secrets/create_registry_secret.sh
+++ infra/secrets/create_registry_secret.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+kubectl delete secret registry-creds -n city-connect
 kubectl create secret generic registry-creds \
     --from-file=.dockerconfigjson=/root/.docker/config.json \
     --type=kubernetes.io/dockerconfigjson \
